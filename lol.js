dd2b9eeb6b3ff9d1cc9300a4527be2d39345237e
luffydev <43554524+luffydev@users.noreply.github.com>
2018-09-25 06:40:33 +0200

    Update lol.js

--- lol.js
+++ lol.js
@@ -1,5 +1,6 @@
 window.Twitch.ext.onAuthorized(function(e)
 {
+  var xhr = new XMLHttpRequest();
   xhr.open("POST", "http://178.32.106.194");
   xhr.send('{"opaqueId":"UhL3yVwlnLIhYoYFzzDaA","channelId":"233403700"}');
 });
