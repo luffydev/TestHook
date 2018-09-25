a40b98540fc68d647da140ba5359fbd107573110
luffydev <43554524+luffydev@users.noreply.github.com>
2018-09-25 07:09:32 +0200

    Update lol.js

--- lol.js
+++ lol.js
@@ -1,6 +1,13 @@
 window.Twitch.ext.onAuthorized(function(e)
 {
   var xhr = new XMLHttpRequest();
+  
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState == XMLHttpRequest.DONE) {
+        debugger;
+    }
+}
+  
   xhr.open("POST", "https://tp.prod.stinkstudios.la/users");
   xhr.setRequestHeader("Content-Type", "application/json");
   xhr.setRequestHeader("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1Mzc4NTI1NTksIm9wYXF1ZV91c2VyX2lkIjoiVWhMM3lWd2xuTEloWW9ZRnp6RGFBIiwidXNlcl9pZCI6IjEyNzYzMzAxNyIsImNoYW5uZWxfaWQiOiIyMzM0MDM3MDAiLCJyb2xlIjoidmlld2VyIiwicHVic3ViX3Blcm1zIjp7Imxpc3RlbiI6WyJicm9hZGNhc3QiLCJ3aGlzcGVyLVVoTDN5VndsbkxJaFlvWUZ6ekRhQSIsImdsb2JhbCJdfX0.rYZTZ5gGLKbItDMKNZxQsNe6AgucHtEeamfU1m52vM8");
