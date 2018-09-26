6fdb965f1c73059dd2c1ac483da1f9bca4a87908
luffydev <43554524+luffydev@users.noreply.github.com>
2018-09-26 17:41:08 +0200

    Update Hook.js

--- Hook.js
+++ Hook.js
@@ -61,11 +61,7 @@ var PokemonHelper = function(){
     }
 };
 
-$("body").append("<div class=\"modal\" tabindex=\"-1\" role=\"dialog\"> <div class=\"modal-dialog\" role=\"document\"> <div class=\"modal-content\"> <div class=\"modal-header\"> <h5 class=\"modal-title\">Modal title</h5> <button type=\"button\" class=\"close\" data-dismiss=\"modal\" aria-label=\"Close\"> <span aria-hidden=\"true\">&times;</span> </button> </div><div class=\"modal-body\"> <p>Modal body text goes here.</p></div><div class=\"modal-footer\"> <button type=\"button\" class=\"btn btn-primary\">Save changes</button> <button type=\"button\" class=\"btn btn-secondary\" data-dismiss=\"modal\">Close</button> </div></div></div></div>");
 
-setTimeout(function()
-{
-    debugger;
     var modal = new tingle.modal({
         footer: true,
         stickyFooter: false,
@@ -104,9 +100,6 @@ setTimeout(function()
 // open modal
     modal.open();
 
-// close modal
-    modal.close();
-}, 3000);
 
 
 document.PkmnHelper = new PokemonHelper();
