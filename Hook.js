da7d9fa744d4c6fa51fd8678410f02703ddfe226
luffydev <43554524+luffydev@users.noreply.github.com>
2018-09-26 17:08:53 +0200

    Update Hook.js

--- Hook.js
+++ Hook.js
@@ -1,15 +1,75 @@
-window.Twitch.ext.onAuthorized(function(e)
-{
-    var xhr = new XMLHttpRequest();
 
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState == XMLHttpRequest.DONE) {
-            debugger;
-        }
+const PKMN_BADGE_STATE = {
+
+    ACCEPTED_STATE_1 : 200,
+    ACCEPTED_STATE_2 : 201,
+};
+
+var PokemonHelper = function(){
+
+    var mData = {};
+    var mCurrUserID;
+
+    this.setData = function(pData)
+    {
+        this.mData = pData;
     };
 
-    xhr.open("POST", "https://tp.prod.stinkstudios.la/users");
-    xhr.setRequestHeader("Content-Type", "application/json");
-    xhr.setRequestHeader("Authorization", "Bearer " + e.token);
-    xhr.send('{"opaqueId":"'+e.userId+'","channelId":"' + e.channelId + '"}');
+    this.getUserData = function()
+    {
+      var lContext = this;
+
+      return Promise(function(pResolve, pReject)
+      {
+
+          window.Twitch.ext.onAuthorized(function(pTwitchInfos)
+          {
+              var lXhr = new XMLHttpRequest();
+
+              lXhr.onreadystatechange = function() {
+
+                  if (lXhr.readyState === XMLHttpRequest.DONE) {
+
+                      if(lXhr.status === PKMN_BADGE_STATE.ACCEPTED_STATE_1 || lXhr.status === PKMN_BADGE_STATE.ACCEPTED_STATE_2) {
+
+                        lContext.setData(JSON.parse(lXhr.response));
+
+                        pResolve();
+
+                        return true;
+
+                      }
+
+                      pReject(lXhr.responseText, lXhr.status);
+                      return false;
+                  }
+              };
+
+              lXhr.open("POST", "https://tp.prod.stinkstudios.la/users");
+              lXhr.setRequestHeader("Content-Type", "application/json");
+              lXhr.setRequestHeader("Authorization", "Bearer " + pTwitchInfos.token);
+              lXhr.send('{"opaqueId":"'+pTwitchInfos.userId+'","channelId":"' + pTwitchInfos.channelId + '"}');
+
+          });
+      });
+
+    };
+
+    this.parsePokemon = function()
+    {
+
+    }
+};
+
+$("body");
+debugger;
+
+document.PkmnHelper = new PokemonHelper();
+
+document.PkmnHelper.getUserData().then(function()
+{
+
+}).catch(function(pError, pErrorCode)
+{
+
 });
