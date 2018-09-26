d726785d1314b15375562c8d0237db2f7b3f5062
luffydev <43554524+luffydev@users.noreply.github.com>
2018-09-26 17:34:20 +0200

    Update Hook.js

--- Hook.js
+++ Hook.js
@@ -66,9 +66,46 @@ $("body").append("<div class=\"modal\" tabindex=\"-1\" role=\"dialog\"> <div cla
 setTimeout(function()
 {
     debugger;
-   var lModal = $('.modal');
-   lModal.modal('show');
+    var modal = new tingle.modal({
+        footer: true,
+        stickyFooter: false,
+        closeMethods: ['overlay', 'button', 'escape'],
+        closeLabel: "Close",
+        cssClass: ['custom-class-1', 'custom-class-2'],
+        onOpen: function() {
+            console.log('modal open');
+        },
+        onClose: function() {
+            console.log('modal closed');
+        },
+        beforeClose: function() {
+            // here's goes some logic
+            // e.g. save content before closing the modal
+            return true; // close the modal
+            return false; // nothing happens
+        }
+    });
 
+// set content
+    modal.setContent('<h1>here\'s some content</h1>');
+
+// add a button
+    modal.addFooterBtn('Button label', 'tingle-btn tingle-btn--primary', function() {
+        // here goes some logic
+        modal.close();
+    });
+
+// add another button
+    modal.addFooterBtn('Dangerous action !', 'tingle-btn tingle-btn--danger', function() {
+        // here goes some logic
+        modal.close();
+    });
+
+// open modal
+    modal.open();
+
+// close modal
+    modal.close();
 }, 3000);
 
 
