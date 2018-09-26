694b8779f74ff16e4b738ee66a9c1cb09b9205f7
luffydev <43554524+luffydev@users.noreply.github.com>
2018-09-26 17:48:49 +0200

    Update Hook.js

--- Hook.js
+++ Hook.js
@@ -61,6 +61,8 @@ var PokemonHelper = function(){
     }
 };
 
+PokemonMessageHandler = function()
+{
 
     var modal = new tingle.modal({
         footer: true,
@@ -69,45 +71,52 @@ var PokemonHelper = function(){
         closeLabel: "Close",
         cssClass: ['custom-class-1', 'custom-class-2'],
         onOpen: function() {
-            console.log('modal open');
+
         },
         onClose: function() {
-            console.log('modal closed');
+
         },
         beforeClose: function() {
             // here's goes some logic
             // e.g. save content before closing the modal
             return true; // close the modal
-            return false; // nothing happens
         }
     });
 
-// set content
-    modal.setContent('<h1>here\'s some content</h1>');
+    this.showMessage = function(pMessage)
+    {
+        // set content
+        modal.setContent(pMessage);
 
 // add a button
-    modal.addFooterBtn('Button label', 'tingle-btn tingle-btn--primary', function() {
-        // here goes some logic
-        modal.close();
-    });
+        modal.addFooterBtn('Button label', 'tingle-btn tingle-btn--primary', function() {
+            // here goes some logic
+            modal.close();
+        });
 
 // add another button
-    modal.addFooterBtn('Dangerous action !', 'tingle-btn tingle-btn--danger', function() {
-        // here goes some logic
-        modal.close();
-    });
+        modal.addFooterBtn('Dangerous action !', 'tingle-btn tingle-btn--danger', function() {
+            // here goes some logic
+            modal.close();
+        });
 
 // open modal
-    modal.open();
+        modal.open();
+    };
+};
 
 
 
-document.PkmnHelper = new PokemonHelper();
 
-document.PkmnHelper.getUserData().then(function()
+
+window.PkmnMessageHandler = new PokemonMessageHandler();
+window.PkmnHelper = new PokemonHelper();
+
+PkmnHelper.getUserData().then(function()
 {
+    PkmnMessageHandler.showMessage("Vous êtes connecté ! =)");
 
 }).catch(function(pError, pErrorCode)
 {
-
+    PkmnMessageHandler.showMessage("Erreur lors de l'authentification auprès de l'extension de Badge. (Erreur " + pErrorCode +" )");
 });
